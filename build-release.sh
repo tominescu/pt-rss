4dc8db133461d3405c5ad7252f5de9daa50dcf16
tominescu <tominescu@gmail.com>
2016-11-13 13:24:21 +0800

    update build-release.sh

--- build-release.sh
+++ build-release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 MD5='md5sum'
 unamestr=`uname`
 if [[ "$unamestr" == 'Darwin' ]]; then
@@ -23,7 +24,7 @@ for os in ${OSES[@]}; do
 		then
 			suffix=".exe"
 		fi
-		env CGO_ENABLED=0 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o pt-rss_${os}_${arch}${suffix} github.com/tominescu/pt-rss/pt-rss
+		env CGO_ENABLED=1 GOOS=$os GOARCH=$arch go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o pt-rss_${os}_${arch}${suffix} github.com/tominescu/pt-rss/pt-rss
 		if $UPX; then upx -9 pt-rss_${os}_${arch}${suffix};fi
 		tar -zcf pt-rss-${os}-${arch}-$VERSION.tar.gz pt-rss_${os}_${arch}${suffix} assets/sample-config.json
 		$MD5 pt-rss-${os}-${arch}-$VERSION.tar.gz
@@ -33,7 +34,7 @@ done
 # ARM
 ARMS=(5 6 7)
 for v in ${ARMS[@]}; do
-	env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=$v go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o pt-rss_linux_arm$v  github.com/tominescu/pt-rss/pt-rss
+	env CGO_ENABLED=1 GOOS=linux GOARCH=arm GOARM=$v go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o pt-rss_linux_arm$v  github.com/tominescu/pt-rss/pt-rss
 done
 if $UPX; then upx -9 pt-rss_linux_arm*;fi
 tar -zcf pt-rss-linux-arm-$VERSION.tar.gz pt-rss_linux_arm* assets/sample-config.json
